ff9d1ea51f92ebcf454409b3028fdd85f31bd0e1
unknown <dkashosi@uco.edu>
2018-02-08 22:47:24 -0600

    End srpint 1 Daniel Kashosi

--- app/src/main/java/edu/uco/avalon/ActivityMain.java
+++ app/src/main/java/edu/uco/avalon/ActivityMain.java
@@ -1,13 +1,109 @@
 package edu.uco.avalon;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class ActivityMain extends AppCompatActivity {
+    int trys =0;
+    Spinner spinner;
+    ArrayAdapter<CharSequence> adp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Login();
+        ForgetPassword();
+        contactUs();
+    }
+
+    private void contactUs() {
+        TextView contact = (TextView) findViewById(R.id.help);
+
+        contact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ActivityMain.this, ActivityContactUs.class));
+            }
+        });
+    }
+
+    private void ForgetPassword() {
+
+        TextView forget = (TextView) findViewById(R.id.txtForget);
+
+        forget.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ActivityMain.this, ActivityResetPassword.class));
+            }
+        });
+    }
+
+    private void Login() {
+        Button login = (Button) findViewById(R.id.btnlogin);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = "dkashosi", password = "Avalon11";
+                String inUser = ((TextView) findViewById(R.id.edit_username)).getText().toString().toLowerCase();
+                String inPassword = ((TextView) findViewById(R.id.edit_password)).getText().toString();
+                if(trys > 2){
+                    new AlertDialog.Builder(ActivityMain.this)
+                            .setTitle("Account Lock")
+                            .setMessage("Your Account has been lock due to many tries please contact the admin to unlock if for you")
+                            .setPositiveButton("Cantact Admin", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    startActivity(new Intent(ActivityMain.this, ActivityContactUs.class));
+
+                                }
+                            })
+                            .setNegativeButton("cancel", null).create().show();
+                }
+                else if (user.equals(inUser))
+                    if(password.equals(inPassword)) {
+
+                    startActivity(new Intent(ActivityMain.this, ActivityHome.class));
+                    }
+                    else{
+                    trys++;
+                    new AlertDialog.Builder(ActivityMain.this)
+                            .setTitle("wrong Password")
+                            .setMessage("You entered an invalid passowrd")
+                            .setPositiveButton("Reset Password", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    startActivity(new Intent(ActivityMain.this, ActivityResetPassword.class));
+
+                                }
+                            })
+                            .setNegativeButton("Try Again", null).create().show();
+                    }
+                else{
+                    new AlertDialog.Builder(ActivityMain.this)
+                            .setTitle("wrong User name")
+                            .setMessage("You entered an invalid userName")
+                            .setPositiveButton("Cantact Admin", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    startActivity(new Intent(ActivityMain.this, ActivityContactUs.class));
+
+                                }
+                            })
+                            .setNegativeButton("Try Again", null).create().show();
+                }
+
+            }
+        });
     }
 }
