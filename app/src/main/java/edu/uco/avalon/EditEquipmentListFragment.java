032fd6acd58e6168ee858c0f66680131193d0bb7
C-Swag <this.better.be.good95@gmail.com>
2018-02-23 01:51:37 -0600

    Callie's Changes (Sprint 2)

--- app/src/main/java/edu/uco/avalon/EditEquipmentListFragment.java
+++ app/src/main/java/edu/uco/avalon/EditEquipmentListFragment.java
@@ -3,6 +3,7 @@ package edu.uco.avalon;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.util.SparseBooleanArray;
 import android.view.ActionMode;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
@@ -10,14 +11,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 /**
  * Created by cdcal on 2/7/2018.
@@ -25,7 +28,9 @@ import java.util.List;
 
 public class EditEquipmentListFragment extends Fragment {
 
-    ListView equipmentList;
+    ListView equipmentListView;
+    TextView emptyText;
+    SimpleAdapter adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -33,9 +38,9 @@ public class EditEquipmentListFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_equipment_list, container, false);
 
-        equipmentList = view.findViewById(R.id.equipmentListView);
-
         //get the equipment list from the database
+        //Start of equipmentListView ***************************************************************
+        equipmentListView = view.findViewById(R.id.equipmentListView);
         String[] from = new String[] {"Name", "Type"};
         int[] to = new int[] { R.id.large_Name, R.id.small_Type};
 
@@ -47,16 +52,14 @@ public class EditEquipmentListFragment extends Fragment {
             map.put("Type", "" + Equipment.equipmentList.get(i).getType());
             fillMaps.add(map);
         }
-
-        SimpleAdapter adapter = new SimpleAdapter(this.getActivity(), fillMaps, R.layout.equipment_list_item, from, to);
-        equipmentList.setAdapter(adapter);
-        equipmentList.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
-        equipmentList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        adapter = new SimpleAdapter(this.getActivity(), fillMaps, R.layout.equipment_list_item, from, to);
+        equipmentListView.setAdapter(adapter);
+        equipmentListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
+        equipmentListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                //pass the id to the api and send the equipment info it gives.
 
-                Toast.makeText(getContext(), "Test", Toast.LENGTH_SHORT).show();
+                //pass the id to the api and send the equipment info it gives.
                 Equipment.editOption = Equipment.equipmentList.get(i); //should get from DB
                 Equipment.id = i;
 
@@ -70,31 +73,94 @@ public class EditEquipmentListFragment extends Fragment {
 
             }
         });
+        equipmentListView.setMultiChoiceModeListener(new AbsListView.MultiChoiceModeListener() {
 
-//        ArrayAdapter<Equipment> adapter = new ArrayAdapter<>(this.getActivity(),
-//                R.layout.equipment_list_item, Equipment.equipmentList);
-//        equipmentList.setAdapter(adapter);
-//        equipmentList.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
-//        equipmentList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-//                //pass the id to the api and send the equipment info it gives.
-//
-//                Equipment.editOption = Equipment.equipmentList.get(i); //should get from DB
-//
-//                Fragment addedFragment = new EditEquipmentFragment();
-//                FragmentTransaction transaction = getFragmentManager().beginTransaction();
-//
-//                transaction.replace(R.id.fragment_container, addedFragment);
-//                transaction.addToBackStack(null);
-//
-//                transaction.commit();
-//
-//            }
-//        });
+            @Override
+            public boolean onActionItemClicked(ActionMode actionMode, MenuItem item) {
+                // Respond to clicks on the actions in the CAB
+                switch (item.getItemId()) {
+                    case R.id.menu_delete:
+                        deleteData();
+                        actionMode.finish(); // Action picked, so close the CAB
+                        return true;
+                    default:
+                        return false;
+                }
+            }
 
+            @Override
+            public boolean onCreateActionMode(ActionMode actionMode, Menu menu) {
+                // Inflate the menu for the CAB
+                MenuInflater inflater = getActivity().getMenuInflater();
+                inflater.inflate(R.menu.menu_equipment_list, menu);
+                return true;
+            }
+
+            @Override
+            public void onDestroyActionMode(ActionMode actionMode) {
+                // Here you can make any necessary updates to the activity when
+                // the CAB is removed. By default, selected items are
+                // deselected/unchecked.
+            }
+
+            @Override
+            public boolean onPrepareActionMode(ActionMode actionMode, Menu menu) {
+                // Here you can perform updates to the CAB due to
+                // an invalidate() request
+                return false;
+            }
+
+            @Override
+            public void onItemCheckedStateChanged(ActionMode actionMode,
+                                                  int position, long id, boolean checked) {
+                // Here you can do something when items are selected/de-selected,
+                // such as update the title in the CAB
+                actionMode.setTitle("(" + equipmentListView.getCheckedItemCount()
+                        + " selected)");
+            }
+        });
+        //End of equipmentListView *****************************************************************
+
+        //Start of emptyText ***********************************************************************
+        emptyText = view.findViewById(R.id.list_emptyText);
+        emptyText.setVisibility((adapter.isEmpty())?View.VISIBLE:View.GONE);
+        //End of emptyText *************************************************************************
 
         return view;
+    }
+
+    private void deleteData() {
+
+        SparseBooleanArray selected = equipmentListView.getCheckedItemPositions();
+        for(int i = selected.size() - 1; i >= 0; i--){
+            if (selected.valueAt(i)) {
+                Equipment.equipmentList.remove(selected.keyAt(i));
+            }
+        }
+
+        //if deletion was successful
+        //Toast.makeText(getContext(), "Equipment Deleted Successfully.", Toast.LENGTH_SHORT).show();
+
+        //get the equipment list from the database. Used to refresh the listview
+        String[] from = new String[] {"Name", "Type"};
+        int[] to = new int[] { R.id.large_Name, R.id.small_Type};
+        List<HashMap<String, String>> fillMaps = new ArrayList<HashMap<String, String>>();
+
+        for(int i = 0; i < Equipment.equipmentList.size(); i++){
+            HashMap<String, String> map = new HashMap<String, String>();
+            map.put("Name", "" + Equipment.equipmentList.get(i).getName());
+            map.put("Type", "" + Equipment.equipmentList.get(i).getType());
+            fillMaps.add(map);
+        }
+        adapter = new SimpleAdapter(this.getActivity(), fillMaps, R.layout.equipment_list_item, from, to);
+        equipmentListView.setAdapter(adapter);
+       // adapter.notifyDataSetChanged(); //doesn't work with SimpleAdapter
+        emptyText.setVisibility((adapter.isEmpty())?View.VISIBLE:View.GONE);
+
+
+
+        //if deletion was unsuccessful
+        //Toast.makeText(getContext(), "Equipment could not be deleted....", Toast.LENGTH_SHORT);
     }
 
 
---
+++ app/src/main/java/edu/uco/avalon/ManageTypesFragment.java
@@ -0,0 +1,136 @@
+package edu.uco.avalon;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class ManageTypesFragment extends Fragment {
+
+    EditText typeEdit;
+    Spinner typeSpinner;
+
+    Button addButton;
+    Button deleteButton;
+
+    String type;
+    ArrayAdapter<String> adapter;
+    int typePos;
+
+
+    public ManageTypesFragment() {
+        // Required empty public constructor
+        type = "";
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_manage_types, container, false);
+
+        typeEdit = view.findViewById(R.id.typeNameEdit);
+
+        //Start of Type Spinner ********************************************************************
+        typeSpinner =  view.findViewById(R.id.typeSpinner);
+        adapter = new ArrayAdapter<>(this.getActivity(),
+                R.layout.spinner_equipment_type, Equipment.typeList);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        typeSpinner.setAdapter(adapter);
+
+        typeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int pos, long id) {
+                type = parent.getItemAtPosition(pos).toString();
+                typePos = pos;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                type = Equipment.typeList.get(0);
+                typePos = 0;
+            }
+        });
+        //End of Type Spinner **********************************************************************
+
+        //Start of Add Button **********************************************************************
+        addButton = view.findViewById(R.id.addTypeButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String typeName = typeEdit.getText().toString().trim();
+                if(!typeName.equals("")) {
+                    updateData(typeName); //update to db
+                }
+                else{
+                    Toast.makeText(getContext(), "Please enter a name for this new type.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        //End of Add Button ************************************************************************
+
+
+        //Start of Delete Button *******************************************************************
+        deleteButton = view.findViewById(R.id.deleteTypeButton);
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                deleteData();
+            }
+        });
+        //End of Delete Button *********************************************************************
+
+
+        return view;
+    }
+
+    protected void updateData(String typeName){
+
+        if(Equipment.typeList.contains(typeName)){
+            Toast.makeText(getContext(), "This type already exists.", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Equipment.typeList.add(typeName); //db
+
+            //if saving was successful
+            Toast.makeText(getContext(), "Saved Successfully.", Toast.LENGTH_SHORT).show();
+            typeSpinner.setSelection(adapter.getPosition(typeName));
+            typeEdit.setText("");
+
+
+            //if saving was unsuccessful, don't go back to previous screen.
+            //Toast.makeText(getContext(), "Saved Unsuccessfully....", Toast.LENGTH_SHORT);
+
+        }
+
+    }
+
+    protected void deleteData(){
+
+        if(Equipment.typeList.size() > 1) {
+            Equipment.typeList.remove(typePos); //db
+
+
+            //if saving was successful
+            Toast.makeText(getContext(), "Deleted Successfully.", Toast.LENGTH_SHORT).show();
+            adapter.notifyDataSetChanged();
+
+            //if saving was unsuccessful, don't go back to previous screen.
+            //Toast.makeText(getContext(), "Saved Unsuccessfully....", Toast.LENGTH_SHORT);
+        }
+        else{
+            Toast.makeText(getContext(), "Cannot delete the single remaining type in the list.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
